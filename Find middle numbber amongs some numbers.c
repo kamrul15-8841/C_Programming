b5ef58902a16425e7c1467867d73342359f776e2
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2022-08-30 00:08:36 +0600

    1

--- Find middle numbber amongs some numbers.c
+++ Find middle numbber amongs some numbers.c
@@ -4,15 +4,15 @@ int main()
     int num1,num2,num3;
     printf("Enter Numbers: ");
     scanf("%d %d %d",&num1,&num2,&num3);
-    if(num2 > num1 && num3 < num2 || num3>num1 && num1>num2)   // 1 2 3
+    if(num2 > num1 && num3 < num2 || num3>num1 && num1>num2)   // 3 2 1 or 3 1 2
     {
         printf("%d",num1);
     }
-    else if(num1>num2 && num2>num3 || num3>num2 && num2>num1)
+    else if(num1>num2 && num2>num3 || num3>num2 && num2>num1) // 1 3 2 or 2 3 1
     {
         printf("%d",num2);
     }
-    else if(num1>num3 && num3>num2 || num2>num3 && num3>num1)
+    else if(num1>num3 && num3>num2 || num2>num3 && num3>num1) // 1 2 3 or 2 1 3
     {
         printf("%d",num3);
     }
