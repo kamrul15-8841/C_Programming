f994626f4c010b3832af0180e093053659cd3d89
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2022-09-09 14:59:30 +0600

    output

---
+++ Output example/array output.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[] = {5,3,4,2,1};
+    for(i=0; i<10;i++)
+    {
+        cout << arr[i] <<" ";
+    }
+}
---
+++ Output example/switch problem.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    for (i=0;i<20;i++)
+    {
+        switch(i)
+        {
+        case 0:
+            i+=5;
+            cout << i << " ";
+
+            case 1:
+            i+=2;
+            cout << i << " ";
+
+            case 5:
+            i+=5;
+            cout << i << " ";
+
+            default:
+            i+=4;
+            cout << i << " ";
+            break;
+
+        }
+
+    }
+     cout << i << " ";
+    return 0;
+}
